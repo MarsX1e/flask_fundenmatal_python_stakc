ddedcb6fb25e027fad8ff062c411349cef610657
MarsX1e <tianyuxie14@gmail.com>
2017-09-09 18:18:29 -0700

    modify hello.py finally make success work

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 
 def hello_world():
     return render_template('index.html')
-app.run(debug=True)
+
 
 @app.route('/success')
 def success():
     return render_template('success.html')
+app.run(debug=True)
 '''
 we created the folder templates. why is the function "render_template"?
 ''' 
+'''
+we are suppose find the success.html in /success. I failed. Why? Do we need add.run(debug=True)?
+problem solved. We need move app.run(debug=True) under the success function
+'''
