5dcb17735224ccb4b2d96cc0e96a134fecb6ee5f
MarsX1e <tianyuxie14@gmail.com>
2017-09-09 17:42:30 -0700

    modify hello.py make it render_templete success.html

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 app.run(debug=True)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+'''
+we created the folder templates. why is the function "render_template"?
+''' 
