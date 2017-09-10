f8c7236f01623cc07bc4704f640e19ed5159d43c
MarsX1e <tianyuxie14@gmail.com>
2017-09-09 20:58:45 -0700

    modify css and js link them to html and run sever

---
+++ flask_fundamentals/hello_flask/static/js/scripts.js
@@ -0,0 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+$(document).ready(function(){
+    $("h1").click(function(){
+        alert("You are not welcome here@")
+    })
+})
+// Never write this in a scripts before. probably I did it wrong
